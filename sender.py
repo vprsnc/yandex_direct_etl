f1bf893450365c80ec66ca18b4c7ebf6b23e2e9d
Georgy <heavypresence@riesup.net>
2023-04-13 16:10:20 +0300

    NotFound exception when del table

--- sender.py
+++ sender.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from google.cloud import bigquery as bq
 from google.api_core.exceptions import BadRequest
+from google.api_core.exceptions import NotFound
 from loguru import logger
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = \
@@ -14,8 +15,11 @@ def send_stats(account):
     df = pd.read_csv(f"tmp_data/{account}.tsv", delimiter="\t")
     table_ref = client.dataset("yandex_oddjob").table(
         f"dw_report_{account}")
-    jobdel = client.delete_table(table_ref)
-    logger.info(jobdel.result())
+    try:
+        jobdel = client.delete_table(table_ref)
+        logger.info(jobdel.result())
+    except NotFound:
+        pass
     jobcre = client.create_table(table_ref)
     logger.info(jobcre.result())
     job = client.load_table_from_dataframe(df, table_ref)
